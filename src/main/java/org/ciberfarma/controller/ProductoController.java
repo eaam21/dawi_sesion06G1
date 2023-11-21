549bc2649f18980e3792fbc55659bcca43d355ed
Elias <elias.alejandro21@gmail.com>
2023-11-20 20:41:26 -0500

    Agregando grafico al reporte

--- src/main/java/org/ciberfarma/controller/ProductoController.java
+++ src/main/java/org/ciberfarma/controller/ProductoController.java
@@ -132,8 +132,23 @@ public class ProductoController {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
+	}
+	
+	@PostMapping(value="/reporte", params="graficar")
+	public void graficar(@RequestParam(name="idCategoria") int idCategoria, HttpServletResponse response) throws JRException, SQLException {
+		String nombreReporte = "grafico_productos";
+		response.setHeader("Content-Disposition", "inline; filename="+nombreReporte+".pdf");
+		response.setContentType("application/pdf");
+		try {
+			String ru = resourceLoader.getResource("classpath:reportes/graficos.jasper").getURI().getPath();
+			HashMap<String, Object>parametros = new HashMap<>();
+			parametros.put("idCategoria", idCategoria);
+			JasperPrint jasperPrint = JasperFillManager.fillReport(ru, parametros, dataSource.getConnection()); 
+			OutputStream outStream = response.getOutputStream();
+			JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	
